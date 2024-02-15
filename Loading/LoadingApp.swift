a0ed347354043facf4ab4db0062e66a8f614737e
Tushar Bendre <tusharbendre01@gmail.com>
2024-02-15 08:06:30 +0530

    Initial Commit

---
+++ Loading/LoadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingApp.swift
+//  Loading
+//
+//  Created by Tushar Bendre on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LoadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
